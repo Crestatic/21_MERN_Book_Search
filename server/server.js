832f8a539e7a365e3a8b43734ef0a3823aaf9b5e
Crestatic <dtnguy86@gmail.com>
2022-12-05 21:27:03 -0800

    impliment the Apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
+// import schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+// create express app
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// crate Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +25,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+
 
 app.use(routes);
 
